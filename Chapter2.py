77679a891d10c5fc4afda7651042d897eafff43d
Playboi-Pirate <demiiboiilegend@gmail.com>
2021-11-02 14:14:20 -0500

    Update Chapter2.py

--- Chapter2.py
+++ Chapter2.py
@@ -36,21 +36,27 @@ Meow.rstrip()
 print(Meow)
 
 #Try it Yourself
-
+print("Try it Yourself")
 #2-3
+print("Question 2-3")
 Jack_Sparrow="Hello Jack,would you like to go on an adventure with me today?"
 #2-4
+print("Question 2-4")
 Pirate="Jack Sparrow"
 print(Pirate.title())
 print(Pirate.upper())
 print(Pirate.lower())
 #2-5
+print("Question 2-5")
 Famous_Quote1='Billy once said,"If my wife ever cheats on me, I will shoot her,the kids, and myself"'
+print(Famous_Quote1)
 #2-6
+print("Question 2-6")
 Famous_Person="I LOVE YOU BILLY.I AM YOUR BIGGEST FAN!"
 message2=f"{Famous_Person}"
 print(message2)
 #2-7
+print("Question 2-7")
 Crazy_Duo="\n\tJack Sparrow\n\tBilly Magnum"
 print(Crazy_Duo)
 print(Crazy_Duo.rstrip())
@@ -58,5 +64,54 @@ print(Crazy_Duo.lstrip())
 print(Crazy_Duo.strip())
 
 #Integers
+print("Number Section")
+print(2+5)
+print(2-5)
+print(2*5)
+print(2/5)
+print(2**5)
+print(2+5*6)
+print((2+5)*6)
+print(0.67+0.76)
+print(0.67-0.76)
+print(0.67*0.76)
+print(0.67/0.76)
+print(0.67**0.76)
+
+#Integer and Floats
+print(4/2) #The float is the decimal after the dot.
+print(1+3.0)
+print(2*3.0)
+print(3.0**2)
+
+#Underscores in Numbers
+Demii=777_777_777
+print(Demii)
+
+#Multiple Assignments
+m,n,b,h=3,9,22,30
+print(m)
+print(b)
+print(n)
+print(h)
+
+#Constants are in all CAPS
+
+#Question 2-8
+print("Question 2-8")
+print(4+4)
+print(10-2)
+print(2*4)
+print(64/8)
+print(2**3)
+
+#Question 2-9
+print("Question 2-9")
+My_Fav_Num=(2+3)*5/5+12-10
+print(My_Fav_Num)
+
+
+
+
 
 
