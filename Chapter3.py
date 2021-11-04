d96601319a696194b36320ca0c18b138d67594fb
Playboi-Pirate <demiiboiilegend@gmail.com>
2021-11-04 14:33:37 -0500

    Update Chapter3.py

--- Chapter3.py
+++ Chapter3.py
@@ -1,1 +1,45 @@
-#Introducing Lists
+#What is a list
+print("What is a list SECTION")
+bicycles=['trek','cannondale','redline','specialized']
+print(bicycles)
+#Accessing elements in a List
+print("Accesing elements in a list Section")
+print(bicycles[0])
+print(bicycles[1])
+print(bicycles[2])
+print(bicycles[3])
+print(f"I like {bicycles[2].title()}")
+#Position starts at 0
+print("Position starts at 0 Section")
+print(bicycles[-1])# This command brings the last element in the list
+#Using Individual Values from a list
+Message=f"My first bike was a {bicycles[-1].title()} Bike"
+print(Message)
+#Try it Youself
+print("Try it yourself Section")
+#3-1
+print("3-1 section")
+names=['Demii','Key','Dey','Donnie','Larry']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+#3-2
+print("3-2 section")
+print(f"You are the coolest,{names[0]}.")
+print(f"I know more about cars than you,{names[1]}.")
+print(f"Want to play the game tonight,{names[2]}.")
+print(f"Sorry I can't workout with you anymore,{names[3]}.")
+print(f"There are some people that you would probably like,{names[4]}.")
+#3-3
+print("3-3 section")
+Cars=['Bugatti','Lam Truck','Rolls-Royce','Miata']
+Speedy=f"I want to drive a {Cars[0]} on the highway as fast as I can"
+Off_Road=f"I want to drive a {Cars[1]} off road for the bumpy ride"
+Luxury=f"I want to drive a {Cars[2]} through the city"
+Drifting=f"I want a {Cars[3]} for drifting everywhere I go"
+print(Speedy)
+print(Off_Road)
+print(Luxury)
+print(Drifting)
